a1bce5efc66f1e6d25aca762c29103760a27ec01
Hiilynn <Teryah@localhost.localdomain>
2018-08-21 17:07:28 +0900

    Update

--- Vector/bench.sh
+++
@@ -1,1 +0,0 @@
-hyperfine --export-markdown ./Bench/bench.md --export-csv ./Bench/bench.csv "./Chapel/bin/stats" "./D/bin/stats" "./Nim/stats" "./Rust/bin/stats" "./Chapel/bin/stats_class" "./D/bin/stats_struct"
